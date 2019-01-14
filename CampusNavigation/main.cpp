ea87b40045af131371490b00788daa3c6132fe5f
Edwardzcn <edwardzcn98@gmail.com>
2019-01-14 10:18:47 +0800

    添加项目文件。

---
+++ CampusNavigation/main.cpp
@@ -0,0 +1,19 @@
+﻿#include "Header.h"
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    Place p[6];
+    string Name[6]={"甲","乙","丙","丁","戊","己"};
+    for(int i=0;i<6;i++)
+    {
+        p[i].name=Name[i];
+        p[i].number=i;
+        p[i].introduction="这是"+p[i].name;
+    }
+    Dijkstra("甲", "己");
+	system("pause");
+	return 0;
+
+}
